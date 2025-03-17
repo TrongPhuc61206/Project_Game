1909783f8419ee29d426e041629ae19e61d79a2b
toanvippro <deptrainhutoan@gmail.com>
2025-03-17 21:11:34 +0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,12 +1,50 @@
+#include "Board.h"
+#include <ctime>
 #include <iostream>
-#include <vector>
-#include <ctime.h>
+#include <cstdlib>
 
-using namespace std;
+int size;
+int board[10][10];
 
-int main(){
-    int n; cin >> n;
-    vector<int> v(n);
+void initBoard(int newsize){
+    srand(time(0));
+    size = newsize;
 
-    return 0;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            board[i][j] = 0;
+        }
+    }
+}
+
+void printBoard(){
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            if(board[i][j] != 0){
+                cout << board[i][j] << "\t";
+            }
+            else{
+                cout << "\t";
+            }
+            cout << endl;
+        }
+    }
+}
+
+void addRandom(){
+    int emptyCells[100][2];
+    int count = 0;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            if(board[i][j] == 0){
+                emptyCells[count][0] = i;
+                emptyCells[count][1] = j;
+                count++;
+            }
+        }
+    }
+    if(count > 0){
+        int n = rand() % count;
+        board[emptyCells[n][0]][emptyCells[n][1]] = (rand() % 10 == 0) ? 4 : 2;
+    }
 }
