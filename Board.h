d8d2efcf405dca45f1f3771ff6d0e3ead9ff867b
toanvippro <deptrainhutoan@gmail.com>
2025-03-17 21:12:14 +0700

    Create Board.h

---
+++ Board.h
@@ -0,0 +1,12 @@
+#ifndef BOARD.H
+#define BOARD.H
+
+using namespace std;
+
+extern int size;
+extern int board[10][10];
+void initBoard(int newsize);
+void addRandom();
+void printBoard();
+
+#endif
