1684eb3b980983abe09a361f632dd44e64fc0a64
TrongPhuc61206 <dtphuc2418@clc.fitus.edu.vn>
2025-03-22 23:39:57 +0700

    Update Input.cpp

--- Input.cpp
+++ Input.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 
+void showInstructions() 
+{
+    cout << "\n";
+    cout << "   HOW TO PLAY \n";
+    cout << "---------------------\n";
+    cout << " W - Move Up\n";
+    cout << " S - Move Down\n";
+    cout << " A - Move Left\n";
+    cout << " D - Move Right\n";
+    cout << " Reach 2048 to win\n";
+    cout << " Invalid keys will be ignored\n";
+    cout << "---------------------\n";
+    cout << "\n";
+}
+
 char getMove() {
     char key;
     while (true) 
