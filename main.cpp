3eaa4312951c2a97dbfc76dd9cb056c7df5e9809
TrongPhuc61206 <dtphuc2418@clc.fitus.edu.vn>
2025-03-22 23:11:29 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include "Board.h"
 #include "Movement.h"
-#include "Input.h"
+#include "input.h"
 
-int main(){
-    cout << "nhap n";
+using namespace std;
+
+int main() 
+{
+    cout << "Nhap n: ";
     int n;
     cin >> n;
 
     initBoard(n);
     printBoard();
-    while(canMove()){
-        char c = getMove();
+
+    while (canMove()) 
+    {
+        char c;
+        do 
+        {
+            c = getMove();
+        } 
+        while (c == '\0'); // Lặp lại nếu nhập sai ký tự
+
         cout << c << endl;
-        if(c == 'w') moveUp();
-        if(c == 's') moveDown();
-        if(c == 'a') moveLeft();
-        if(c == 'd') moveRight();
+        if (c == 'w') moveUp();
+        if (c == 's') moveDown();
+        if (c == 'a') moveLeft();
+        if (c == 'd') moveRight();
+
         addRandom();
         printBoard();
     }
 
+    cout << "Game Over!" << endl;
     return 0;
 }
