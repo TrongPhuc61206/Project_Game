7df7ee2d45a2cac8fb8f410750bd90c4f6cbdf03
TrongPhuc61206 <dtphuc2418@clc.fitus.edu.vn>
2025-03-22 23:10:32 +0700

    Update Input.cpp

--- Input.cpp
+++ Input.cpp
@@ -1,16 +1,23 @@
-#include "Input.h"
+#include "input.h"
 #include <conio.h>
 #include <iostream>
 
 using namespace std;
 
-char getMove(){
+char getMove() {
     char key;
-    key = _getch();// Đọc phím từ người chơi
-    if(key == 'w' || key == 'W') return 'w';
-    if(key == 's' || key == 'S') return 's';
-    if(key == 'a' || key == 'A') return 'a';
-    if(key == 'd' || key == 'D') return 'd';
+    while (true) 
+    { 
+        key = _getch(); // Đọc phím từ người chơi
+        key = tolower(key); // Chuyển về chữ thường để dễ xử lý
 
-    return '\0'; // Nếu nhập phím không hợp lệ
+        if (key == 'w' || key == 'a' || key == 's' || key == 'd') 
+        {
+            return key;
+        } 
+        else 
+        {
+            cout << endl << "Invalid key! Please enter W, A, S or D" << endl;
+        }
+    }
 }
