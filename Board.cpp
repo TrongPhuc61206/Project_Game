5f6710ad7cfb340a431156a822d02334b3eb9e98
TrongPhuc61206 <dtphuc2418@clc.fitus.edu.vn>
2025-03-23 22:39:18 +0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -3,54 +3,56 @@
 #include <iostream>
 #include <cstdlib>
 
-int size;
+
+int boardSize;
 int board[10][10];
 
-// Khởi tạo bảng chơi 2048
+
+// Initialize 2048 board
 void initBoard(int newsize){
     srand(time(0));
-    size = newsize;
+    boardSize = newsize;
 
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
+    for(int i = 0; i < boardSize; i++){
+        for(int j = 0; j < boardSize; j++){
             board[i][j] = 0;
         }
     }
 
-    //Thêm 2 ô số ngẫu nhiên vào bảng
+    // Add 2 random number cells to the table
     addRandom();
     addRandom();
 }
 
-// Hiển thị bảng chơi
+// Show game board
 void printBoard() {
-    system("cls"); // Xóa màn hình 
+    system("cls"); // Clear screen 
     cout << "+";
-    for (int i = 0; i < size; i++) cout << "------+"; // Viền ngang trên
+    for (int i = 0; i < boardSize; i++) cout << "------+"; // Top horizontal border
     cout << endl;
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
             cout << "|";
             if (board[i][j] == 0)
-                cout << "      "; // Ô trống
+                cout << "      "; // Blank cell
             else
                 cout << " " << board[i][j] << string(5 - to_string(board[i][j]).length(), ' ');
         }
         cout << "|" << endl;
 
         cout << "+";
-        for (int j = 0; j < size; j++) cout << "------+"; // Viền ngang dưới
+        for (int j = 0; j < boardSize; j++) cout << "------+"; // Bottom border
         cout << endl;
     }
 }
 
-// Thêm ô số ngẫu nhiên
+// Add random number box
 void addRandom(){
     int emptyCells[100][2];
     int count = 0;
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
+    for(int i = 0; i < boardSize; i++){
+        for(int j = 0; j < boardSize; j++){
             if(board[i][j] == 0){
                 emptyCells[count][0] = i;
                 emptyCells[count][1] = j;
