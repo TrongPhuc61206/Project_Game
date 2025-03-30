069fc0ce6729e5eed677a622a74ab4b48dec273e
TrongPhuc61206 <dtphuc2418@clc.fitus.edu.vn>
2025-03-30 11:36:12 +0700

    Create UserAuth.h

---
+++ UserAuth.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+
+struct User {
+    std::string username;
+    std::string passwordHash;
+    User* left;
+    User* right;
+};
+
+class UserAuth {
+public:
+    UserAuth();
+    ~UserAuth();
+    void registerUser(const std::string& username, const std::string& password);
+    bool loginUser(const std::string& username, const std::string& password);
+    void saveUsersToFile();
+    void loadUsersFromFile();
+
+private:
+    User* root;
+    void insert(User*& node, const std::string& username, const std::string& passwordHash);
+    bool search(User* node, const std::string& username, const std::string& password);
+    void deleteTree(User* node);
+};
