c3b01fc12345db48630c29b7b61f8d7b8dd31e02
TrongPhuc61206 <dtphuc2418@clc.fitus.edu.vn>
2025-03-30 11:37:21 +0700

    Update Input.cpp

--- Input.cpp
+++ Input.cpp
@@ -1,4 +1,6 @@
 #include "input.h"
+#include "UndoRedo.h"
+#include "Board.h"
 #include <conio.h>
 #include <iostream>
 
@@ -13,6 +15,8 @@ void showInstructions()
     cout << " S - Move Down\n";
     cout << " A - Move Left\n";
     cout << " D - Move Right\n";
+    cout << " Z - Undo\n";
+    cout << " Y - Redo\n";
     cout << " Reach 2048 to win\n";
     cout << " Invalid keys will be ignored\n";
     cout << "---------------------\n";
@@ -23,16 +27,30 @@ char getMove() {
     char key;
     while (true) 
     { 
-        key = _getch(); // Read keys from player
+        key = getch(); // Read keys from player
         key = tolower(key); // Convert to lowercase for ease of handling
 
         if (key == 'w' || key == 'a' || key == 's' || key == 'd') 
         {
             return key;
         } 
-        else 
+        else if (key == 'z') 
         {
-            cout << endl << "Invalid key! Please enter W, A, S or D" << endl;
+            undoMove();
+            printBoard();  
+            showInstructions();
+            continue;
+        } 
+        else if (key == 'y') 
+        {
+            redoMove();
+            printBoard(); 
+            showInstructions();
+            continue;
+        }
+        else
+        {
+            cout << endl << "Invalid key! Please enter W, A, S, D, Z or Y" << endl;
         }
     }
 }
