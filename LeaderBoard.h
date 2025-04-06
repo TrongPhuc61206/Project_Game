309c81dddfb79f1f2604c9b717a012452b6a388f
toanvippro <deptrainhutoan@gmail.com>
2025-04-06 20:21:36 +0700

    Update LeaderBoard.h

--- LeaderBoard.h
+++ LeaderBoard.h
@@ -22,6 +22,5 @@ Node* insertNode(Node* root, const player &data);
 void PrintLeaderBoard(Node* root);
 void saveLeaderboard(Node* root, ofstream &outfile);
 Node* loadLeaderboardFromFile(const char* filename);
-void find(Node* root, Node* &temp, const string &name);
-Node* findPlayer(Node* root, const string &name);
+void freeTree(Node* root);
 
