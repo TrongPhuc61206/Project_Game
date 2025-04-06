fed5df834630043ee2a30ed93c6851caa471c954
toanvippro <deptrainhutoan@gmail.com>
2025-04-06 20:15:34 +0700

    Update SaveGame.cpp

--- SaveGame.cpp
+++ SaveGame.cpp
@@ -3,21 +3,35 @@
 #include <fstream>
 #include "SaveGame.h"
 
-void ReadGame(const string &filename){
- 
-    ifstream file(filename);
+void ReadGame(const string &filename) 
+{
+    ifstream file;
+    file.open(filename);
+    if (!file.is_open()) {
 
-    for(int i = 0; i < boardSize; i++){
-        for(int j = 0; j < boardSize; j++){
+    }
+    file >> boardSize;
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
             file >> board[i][j];
         }
     }
     file.close();
+
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
+
 
 void SaveGame(const string &filename){
 
     ofstream file(filename);
+    file << boardSize;
+    file << "\n";
 
     for(int i = 0; i < boardSize; i++){
         for(int j = 0; j < boardSize; j++){
@@ -28,8 +42,3 @@ void SaveGame(const string &filename){
     file.close();
 }
 
-void ClearFile(const string &filename) {
-    ofstream file(filename, ios::trunc);
-    file.close();
-
-}
