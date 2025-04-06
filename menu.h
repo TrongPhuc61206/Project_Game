cf7f78ce227ee421609f33d6c64e83c870c2d22d
TrongPhuc61206 <dtphuc2418@clc.fitus.edu.vn>
2025-04-06 19:28:32 +0700

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,21 +1,30 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
 #include <vector>
+#include <string>
 
- 1. Xử lý logic trò chơi
-void initGame(int &boardSize, std::vector<std::vector<int>> &board);
-void drawBoard(sf::RenderWindow &window, const std::vector<std::vector<int>> &board, int boardSize);
-void drawGameOver(sf::RenderWindow &window);
+// Constants
+const int MAX_BOARD_SIZE = 10;
+const int MIN_BOARD_SIZE = 4;
 
- 2. Giao diện chính (Menu)
+// Global Variables
+extern sf::Font font;
+extern sf::Text newGameText, resumeGameText, settingsText, leaderboardText, backText, boardSizeText;
+
+// Function Declarations
+void initGame(int& boardSize, std::vector<std::vector<int>>& board);
+void drawBoard(sf::RenderWindow& window, const std::vector<std::vector<int>>& board, int boardSize);
+void drawGameOver(sf::RenderWindow& window);
+
 void initMenu();
-void drawMainMenu(sf::RenderWindow &window);
-void handleMenuInput(sf::Event &event, sf::RenderWindow &window, bool &gameRunning, bool &inMenu, 
-                     bool &newGame, bool &resumeGame, bool &inSettings);
+void drawMainMenu(sf::RenderWindow& window);
+void handleMenuInput(sf::Event& event, sf::RenderWindow& window, bool& gameRunning, bool& inMenu,
+                     bool& newGame, bool& resumeGame, bool& inSettings, bool& inLeaderboard);
 
-3. Màn hình Cài Đặt (Settings)
-void drawSettingsMenu(sf::RenderWindow &window, int &boardSize);
-void handleSettingsInput(sf::Event &event, sf::RenderWindow &window, int &boardSize, bool &inSettings);
+void drawSettingsMenu(sf::RenderWindow& window, int& boardSize);
+void handleSettingsInput(sf::Event& event, sf::RenderWindow& window, int& boardSize, bool& inSettings);
+
+void drawLeaderboard(sf::RenderWindow& window);
+void handleLeaderboardInput(sf::RenderWindow& window, bool& inLeaderboard);
+
