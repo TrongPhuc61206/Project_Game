eadab41b20e3d1763c52d81d08884f350aa4f6d6
TrongPhuc61206 <dtphuc2418@clc.fitus.edu.vn>
2025-04-06 16:29:20 +0700

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <vector>
+
+ 1. Xử lý logic trò chơi
+void initGame(int &boardSize, std::vector<std::vector<int>> &board);
+void generateTile(std::vector<std::vector<int>> &board, int boardSize);
+void drawBoard(sf::RenderWindow &window, const std::vector<std::vector<int>> &board, int boardSize);
+void drawGameOver(sf::RenderWindow &window);
+
+ 2. Giao diện chính (Menu)
+void initMenu();
+void drawMainMenu(sf::RenderWindow &window);
+void handleMenuInput(sf::Event &event, sf::RenderWindow &window, bool &gameRunning, bool &inMenu, 
+                     bool &newGame, bool &resumeGame, bool &inSettings);
+
+3. Màn hình Cài Đặt (Settings)
+void drawSettingsMenu(sf::RenderWindow &window, int &boardSize);
+void handleSettingsInput(sf::Event &event, sf::RenderWindow &window, int &boardSize, bool &inSettings);
