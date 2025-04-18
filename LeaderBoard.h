5fbfed06b54cb43fe7133fd11c15a0fe98f6df94
toanvippro <deptrainhutoan@gmail.com>
2025-04-18 15:05:55 +0700

    Update LeaderBoard.h

--- LeaderBoard.h
+++ LeaderBoard.h
@@ -1,26 +1,29 @@
 #pragma once
-#include <iostream>
-#include <iostream>
-#include <fstream>
+
+#include <string>
+#include <fstream>  
+#include <iostream> 
 
 using namespace std;
 
-struct player{
+struct player {
     int Score;
     string name;
 };
 
-struct Node{
+struct Node {
     player key;
     Node* left;
     Node* right;
 };
 
-player data(int score, const string &name);
-Node* createNode(const player &data);
-Node* insertNode(Node* root, const player &data);
-void PrintLeaderBoard(Node* root);
-void saveLeaderboard(Node* root, ofstream &outfile);
+
+
+player data(int score, const string& name);
+Node* createNode(const player& data);
+Node* insertNode(Node* root, const player& data);
+void saveLeaderboard(Node* root, ofstream& outfile);
 Node* loadLeaderboardFromFile(const char* filename);
 void freeTree(Node* root);
-
+void DrawLeaderBoard();
+void Traversal(Node* root);
